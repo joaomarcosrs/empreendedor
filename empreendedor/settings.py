d3f6dc82b3b889ef0e2ce80a8a539d25d0f60631
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-28 20:28:00 -0300

    Configuração heroku

--- empreendedor/settings.py
+++ empreendedor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
