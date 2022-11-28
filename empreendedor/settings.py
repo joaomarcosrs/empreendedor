7f6462307ff8b50fdcd6b42c74566ea935ad39ca
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-28 20:43:29 -0300

    Consertando Erro (500)

--- empreendedor/settings.py
+++ empreendedor/settings.py
@@ -141,6 +141,22 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
